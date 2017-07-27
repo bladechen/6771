283bc574299e8f21075880046eef5072008cb009
bladechen <475979248@qq.com>
2017-07-27 06:18:13 +0000

    hello_world

---
+++ helloworld/hello_world.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include "func.h"
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl;
+
+
+    my_add(1, 3);
+    vector<int> a;
+    a.push_back(1);
+    // my_add();
+
+    return 0;
+}

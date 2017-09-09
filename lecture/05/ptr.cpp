aaa477df2e2169154d37fe6a4e3ca06651cb960b
bladechen <475979248@qq.com>
2017-09-09 10:35:47 +0000

    fix make

--- lecture/05/ptr.cpp
+++ lecture/05/ptr.cpp
@@ -1,4 +1,7 @@
 #include <cassert>
+#include <cstring>
+#include <vector>
+#include <time.h>
 #include <memory>
 #include <iostream>
 class PointPtr;
@@ -136,10 +139,61 @@ class MyType
         {
             std::cout << "free MyType\n";
         }
-        int  i ;
-        std::unique_ptr<MyType> next;
+        int  i {100};
+        // std::unique_ptr<MyType> next;
 };
 
+void func(std::unique_ptr<MyType> p)
+{
+    std::cout << "func uniq: " <<  std::endl;
+    (void)p;
+}
+
+
+
+void func(std::shared_ptr<MyType> p)
+{
+    std::cout << "func share: " <<  std::endl;
+    (void)p;
+}
+// void func1()
+// {
+//     std::cout << "func1: " <<  std::endl;
+//     (void)p;
+// }
+
+
+std::vector<MyType> g_v;
+void test_vec()
+{
+    MyType a;
+    std::cout << "test_vec addr: " << &a << std::endl;
+    g_v.push_back(a);
+    g_v.emplace_back(std::move(a));
+}
+
+auto test_lambda()
+{
+    int b = 10;
+    auto foo = [b] (void){
+        // b = g_v.size() * 10 + 9;
+        std::cout << "i am " << b << std::endl;
+    };
+    foo();
+    return foo;
+}
+
+
+
+
+
+void clear_stack()
+{
+    char buf[1000];
+    memset(buf, 0, sizeof (buf));
+    std::cout << buf[10] << std::endl;
+
+}
 int main() {
     // ptr<A>*p = new ptr<A>(new A());
     // p->counter  = 10;
@@ -150,9 +204,59 @@ int main() {
     x = NULL;
     x = NULL;
 
-    auto p = std::make_unique<MyType>();
-    p->next = std::move(p);
-    int * leak_ptr = new int(1000);
-    std::cout << *leak_ptr + 100 << std::endl;
+    // auto p = std::make_unique<MyType>();
+    //
+    //
+    // p.reset(p.release());
+    //
+    // auto ui = std::make_unique<MyType>();
+    // func(std::move(ui));
+    // // p->next = std::move(p);
+    // // int * leak_ptr = new int(time(NULL));
+    // // std::cout << *leak_ptr + 100 << std::endl;
+    // std::shared_ptr<int> xs = std::make_shared<int>(1);
+    // std::weak_ptr<int> wp = xs;
+    // xs = NULL;
+    // // xs.reset();
+    // auto xs1 = wp.lock();
+    // std::cout << wp.lock() << std::endl;
+    // std::cout << xs1 << std::endl;
+
+
+    {
+        std::weak_ptr<MyType> weak ;
+        {
+            auto p1 = std::make_shared<MyType>();
+            // auto p2 = std::make_shared<MyType>();
+
+            std::shared_ptr<MyType> p2(new MyType());
+            weak = p1;
+            std::cout << "p1 goes out of scope" << std::endl;
+        }
+        std::cout << "wk goes out of scope" << std::endl;
+    }
+     // p1 = NULL;
+     // p2 = NULL;
+     // std::weak_ptr<int> weak =
+     //
+
+
+    std::cout<< " calling func" << std::endl;
+    func(std::unique_ptr<MyType>(new MyType()));
+    std::cout<< " end func" << std::endl;
+
+    std::shared_ptr<MyType> p = std::make_shared<MyType>();
+    func(std::move(p));
+
+
+    test_vec();
+    std::cout << g_v.size() << "\n";
+    std::cout << "test_vec addr: " << &(g_v[0]) << std::endl;
+
+
+    auto fff = test_lambda();
+    // clear_stack();
+    fff();
     std::cout << "finish main!!!!\n";
+
 }
